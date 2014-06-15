98ce8c65309f8ba94d22ffab830aeca20fd326e5
tonymanou <tonymanou@gmail.com>
2014-06-15 19:44:09 +0200

    Fixed text filter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,18 @@
 
 angular.module('webdesignApp')
   .controller('MainCtrlFr', function ($scope, $http) {
+    $scope.search = {
+      'text': null
+    };
+
+    // Load language data
     $http.get(
         'data/lang.fr.json'
       ).success(function(data) {
         $scope.lang = data;
       });
+
+    // Load resume data
     $http.get(
         'data/resume.fr.json'
       ).success(function(data) {
@@ -16,11 +23,18 @@ angular.module('webdesignApp')
 
 angular.module('webdesignApp')
   .controller('MainCtrlEn', function ($scope, $http) {
+    $scope.search = {
+      'text': null
+    };
+
+    // Load language data
     $http.get(
         'data/lang.en.json'
       ).success(function(data) {
         $scope.lang = data;
       });
+
+    // Load resume data
     $http.get(
         'data/resume.en.json'
       ).success(function(data) {
