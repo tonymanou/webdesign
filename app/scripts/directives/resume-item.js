1f9d953b15ad4d4e7de2a30116b436d5a075e4d1
tonymanou <tonymanou@gmail.com>
2014-06-15 20:00:51 +0200

    Added dummy directive

---
+++ app/scripts/directives/resume-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('webdesignApp')
+  .directive('resumeItem', function() {
+    return {
+      restrict: 'AEC',
+      templateUrl: 'views/resume-item.html'
+    }
+});
